f82aae1df4b6e9ec18cca5ffbe25d5c88ad7a6e7
adarshkhare <adarshkhare@gmail.com>
2017-03-19 13:40:16 -0700

    Added LinkedList Example

--- src/main/java/AlgorithmSamples/LinkedListManipulation.java
+++ src/main/java/AlgorithmSamples/LinkedListManipulation.java
@@ -1,9 +1,36 @@
 package AlgorithmSamples;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
 /**
  * Created by adarshkhare on 3/19/17.
  */
 
 public class LinkedListManipulation
 {
-    
+    public static<T> T GetNthElementFromEndOfTheList(LinkedList<T> list, int nElement)
+    {
+        T returnElement = null;
+        if (list != null )
+        {
+            ListIterator<T> baseIterator = list.listIterator();
+            ListIterator<T> endIterator;
+            for (int i = 0; i < nElement; i++)
+            {
+                if (baseIterator.hasNext())
+                    returnElement = baseIterator.next();
+                else //Not enough elements in Linked List
+                    returnElement = null;
+            }
+            endIterator = baseIterator;
+            baseIterator = list.listIterator();
+            while (endIterator.hasNext())
+            {
+                returnElement = baseIterator.next();
+                endIterator.next();
+            }
+        }
+        return returnElement;
+    }
 }
---
+++ src/test/java/LinkedListManipulation/Test/NthElementFromEnd.java
@@ -0,0 +1,78 @@
+package LinkedListManipulation.Test;
+
+import AlgorithmSamples.LinkedListManipulation;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+
+/**
+ * Created by adarshkhare on 3/19/17.
+ */
+public class NthElementFromEnd {
+    @Test
+    void nThElementFromEndBasic()
+    {
+        Integer[] testArray = new Integer[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        LinkedList<Integer> testList = GetTestList(testArray);
+        int nElement = 0;
+        Integer result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertEquals(0, result.longValue(), "Last element");
+
+        nElement = 1;
+        result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertEquals(1, result.longValue(), "Second Last element");
+
+        nElement = 9;
+        result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertEquals(9, result.longValue(), "First Element");
+
+        nElement = 5;
+        result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertEquals(5, result.longValue(), "Middle Element");
+    }
+
+
+    @Test
+    void nThElementFromNullList() {
+        LinkedList<Integer> testList = null;
+        int nElement = 10;
+        Integer result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertNull(result, "Null list should return null");
+
+        nElement = 0;
+        result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertNull(result, "Null list should return null");
+    }
+
+    @Test
+    void nThElementFromListWhereIndexOutOfrange() {
+        Integer[] testArray = new Integer[]{9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
+        LinkedList<Integer> testList = GetTestList(testArray);
+        int nElement = 20;
+        Integer result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertNull(result, "OutOfRange should return null");
+    }
+
+    @Test
+    void nThElementFromListWhereListIsEmpty() {
+        Integer[] testArray = new Integer[]{};
+        LinkedList<Integer> testList = GetTestList(testArray);
+        int nElement = 0;
+        Integer result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertNull(result, "Empty list should return null");
+
+        nElement = 10;
+        result = LinkedListManipulation.GetNthElementFromEndOfTheList(testList, nElement);
+        Assertions.assertNull(result, "Empty list should return null");
+    }
+
+
+    private LinkedList<Integer> GetTestList(Integer[] testArray)
+    {
+        LinkedList<Integer> testList = new LinkedList<Integer>();
+        testList.addAll(Arrays.asList(testArray));
+        return testList;
+    }
+}
